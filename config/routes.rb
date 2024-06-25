b46f2ad1a215a867bc468b9cfe90f3430035b163
borvux <bennyjoram@email.com>
2024-06-25 11:42:02 -0500

    named the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,28 @@ Rails.application.routes.draw do
 
   # CREATE
   # post("/movies", { :controller => "movies", :action => "create" })
-  post "/movies" => "movies#create"
+  post "/movies" => "movies#create", as: :movies
+
   # get("/movies/new", { :controller => "movies", :action => "new" })
-  get "/movies/new" => "movies#new"
+  get "/movies/new" => "movies#new", as: :new_movie
           
   # READ
   # get("/movies", { :controller => "movies", :action => "index" })
-  get "/movies" => "movies#index"
+  get "/movies" => "movies#index" # this one is already named as movies 
+
   # get("/movies/:id", { :controller => "movies", :action => "show" })
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie
   
   # UPDATE
   # patch("/movies/:id", { :controller => "movies", :action => "update" })
-  patch "/movies/:id" => "movies#update"
+  patch "/movies/:id" => "movies#update" # this one is already named as movie
+
   # get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
-  get "movies/:id/edit" => "movies#edit"
+  get "movies/:id/edit" => "movies#edit", as: :edit_movie 
   
   # DELETE
   # delete("/movies/:id", { :controller => "movies", :action => "destroy" })
-  delete "movies/:id" => "movies#destroy"
+  delete "movies/:id" => "movies#destroy" # this one is already named as movie
 
   #------------------------------
 end
