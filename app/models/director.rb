4a4f5efff7142310608da6a5ac8022fc899ee94b
borvux <bennyjoram@email.com>
2024-06-26 16:01:51 -0500

    generated director model

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :bigint           not null, primary key
+#  bio        :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
 end
