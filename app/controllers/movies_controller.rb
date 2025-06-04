06cfa565fdaecae897d71a579760bf2f70cee20a
borvux <bennyjoram@gmail.com>
2025-06-04 13:08:54 -0500

    solution part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,39 +1,34 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
-    matching_movies = Movie.all
-
-    @list_of_movies = matching_movies.order({ created_at: :desc })
+    @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
-      format.html do
-        render "movies/index"
-      end
+      format.html
     end
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ id: the_id })
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id)) # @movie = Movie.find_by(id: params.fetch(:id)) # @movie = Movie.where(id: params.fetch(:id)).first
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
-    if @the_movie.valid?
-      @the_movie.save
+    # @movie = Movie.new
+    # @movie.title = params.fetch(:title)
+    # @movie.description = params.fetch(:description)
+
+    if @movie.valid?
+      @movie.save
       redirect_to movies_url, { notice: "Movie was successfully created." } # we should only use paths on the client facing view templates. But, when we’re sending responses back, we should use the fully qualified URL, including the domain name.
     else
       render "movies/new"
@@ -41,33 +36,28 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ id: the_id })
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ id: the_id }).first
+    @movie = Movie.find(params.fetch(:id))
+    movie_attributes = params.require(:movie).permit(:title, :description)
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    # movie.title = params.fetch(:title)
+    # movie.description = params.fetch(:description)
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to movie_url(the_movie), { notice: "Movie updated successfully." }
+    if @movie.update(movie_attributes)
+      @movie.save
+      redirect_to movie_url(@movie), { notice: "Movie updated successfully." }
     else
-      redirect_to movie_url(the_movie), { alert: "Movie failed to update successfully." }
+      redirect_to movie_url(@movie), { alert: "Movie failed to update successfully." }
     end
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ id: the_id }).first
+    movie = Movie.find(params.fetch(:id))
 
-    the_movie.destroy
+    movie.destroy
 
     redirect_to movies_url, { notice: "Movie deleted successfully." }
   end
